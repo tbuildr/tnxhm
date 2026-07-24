ec7cb0777219e78367a8e0273f400061ed41aacf
Tom <36055946+tbuildr@users.noreply.github.com>
2026-07-25 00:21:42 +0100

    Added tide shell

--- features/shell.nix
+++ features/shell.nix
@@ -1,35 +1,12 @@
-{
-  self,
-  pkgs,
-  ...
-}: {
+{self, ...}: {
   flake.homeModules.shell = {
-    xdg.configFile = {
-      "starship/starship.toml".source =
-        self + /dotfiles/starship/starship.toml;
-
-      "starship/starship-toolbox.toml".source =
-        self + /dotfiles/starship/starship-toolbox.toml;
-    };
-
+    pkgs,
+    config,
+    lib,
+    ...
+  }: {
     programs.fish = {
       enable = true;
-
-      /*
-      shellInit = ''
-        # Preserve STARSHIP_CONFIG supplied by devenv.
-        # devenv exports its own STARSHIP_CONFIG before starting its fish subshell.
-        if set -q DEVENV_ROOT
-          # Preserve the project-specific devenv configuration.
-        else if set -q TOOLBOX_PATH
-          # Toolbox shells inherit the host value, so override it explicitly.
-          set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship-toolbox.toml"
-        else
-          # Normal host shell.
-          set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
-        end
-      '';
-      */
 
       plugins = [
         {
@@ -61,18 +38,6 @@
         echo
       '';
     };
-
-    /*
-    programs.starship = {
-      enable = true;
-      enableFishIntegration = true;
-
-      settings = {
-        add_newline = false;
-        aws.disabled = true;
-      };
-    };
-    */
 
     programs.zoxide = {
       enable = true;
