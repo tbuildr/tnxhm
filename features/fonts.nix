17111714cb971a9847fc0c5b58ed243e3c4b6f64
Tom <36055946+tbuildr@users.noreply.github.com>
2026-07-25 19:26:31 +0100

    Added Meslo Nerd fonts

--- features/fonts.nix
+++ features/fonts.nix
@@ -2,6 +2,7 @@
   flake.homeModules.fonts = {pkgs, ...}: {
     home.packages = [
       pkgs.nerd-fonts.jetbrains-mono
+      pkgs.nerd-fonts.meslo-lg
     ];
 
     fonts.fontconfig.enable = true;
