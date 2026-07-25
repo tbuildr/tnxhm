52d851c21e6415c0ead27d1a648ec9cc32586c26
Tom <36055946+tbuildr@users.noreply.github.com>
2026-07-25 01:21:13 +0100

    removed starship dotfiles config. Added devenv tide prompt color change function

--- features/shell.nix
+++ features/shell.nix
@@ -15,6 +15,25 @@
         }
       ];
 
+      functions = {
+        __devenv_prompt_color = {
+          body = ''
+            if set -q IN_NIX_SHELL
+                set -g tide_os_bg_color 0D2847
+                set -g tide_pwd_bg_color 1E5A8E
+                set -g tide_cmd_duration_bg_color 4A90D9
+                set -g tide_time_bg_color D6E9F8
+            else
+                set -ge tide_os_bg_color
+                set -ge tide_pwd_bg_color
+                set -ge tide_cmd_duration_bg_color
+                set -ge tide_time_bg_color
+            end
+          '';
+          onVariable = "PWD";
+        };
+      };
+
       shellAliases = {
         ls = "eza --icons --group-directories-first";
         ll = "eza -lah --icons --group-directories-first --git";
@@ -36,6 +55,8 @@
 
         fastfetch
         echo
+
+        __devenv_prompt_color
       '';
     };
 
