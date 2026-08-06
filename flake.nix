10be5ccb21609d67e3d469b1b6e54565667d948d
Tom <36055946+tbuildr@users.noreply.github.com>
2026-08-06 01:53:48 +0100

    Switched to unstable from 26.05 in flake.nix

--- flake.nix
+++ flake.nix
@@ -14,10 +14,10 @@
   */
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
