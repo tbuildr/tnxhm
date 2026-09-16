8c701a4eacb97934d6b7c8a6c06c18b01603421a
Tom <36055946+tbuildr@users.noreply.github.com>
2026-09-17 00:43:39 +0100

    Switched to direnv hook to allow use of local .envrc files

--- features/devenv-setup.nix
+++ features/devenv-setup.nix
@@ -7,11 +7,22 @@
     # Install the Devenv CLI for the user.
     home.packages = [
       pkgs.devenv
+      pkgs.direnv
     ];
 
+    # Global direnvrc: pulls in devenv's `use_devenv` function so any
+    # project's .envrc can just say `use devenv` without repeating the
+    # source_url boilerplate per-project.
+    xdg.configFile."direnv/direnvrc".text = ''
+      eval "$(devenv direnvrc)"
+    '';
+
     # Automatically activate trusted Devenv projects.
+    # Changed to call direnv which it's envrc will call devenv
+    # This means that we can have .envrc in project subdirs to set variables specific to that
+    # and hold that next to the project
     programs.fish.interactiveShellInit = lib.mkAfter ''
-      devenv hook fish | source
+      direnv hook fish | source
     '';
   };
 }
